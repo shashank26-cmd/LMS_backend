9620e075c8def33ce5dd2c695a11a8ad8d4e2cab
SMishra <smishra@kenmarkitan.com>
2024-05-20 00:46:56 +0530

    made code proper to run

--- index.js
+++ index.js
@@ -1,35 +1,24 @@
-import cloudinary  from 'cloudinary';
-import Razorpay from 'razorpay';
-import app from './app.js';
-import connectionToDB from './config/db.js';
-import {config} from 'dotenv'
+import cloudinary from "cloudinary";
+import Razorpay from "razorpay";
+import app from "./app.js";
+import connectionToDB from "./config/db.js";
+import { config } from "dotenv";
 config();
-
 
 // Cloudinary configuration
 cloudinary.v2.config({
-  cloud_name: process.env.Cloud_Name, 
-  api_key: process.env.Cloud_Key, 
-  api_secret:process.env.Cloud_Secret
-
-  
+  cloud_name: process.env.Cloud_Name,
+  api_key: process.env.Cloud_Key,
+  api_secret: process.env.Cloud_Secret,
 });
 
-
-
 export const razorpay = new Razorpay({
-
-key_id: process.env.RAZORPAY_KEY_ID,
-key_secret:process.env.RAZORPAY_SECRET,
-// plan_id:process.env.RAZORPAY_PLAN_ID 
-
-
-})
-
-
+  key_id: process.env.RAZORPAY_KEY_ID,
+  key_secret: process.env.RAZORPAY_SECRET,
+  // plan_id:process.env.RAZORPAY_PLAN_ID
+});
 
 const PORT = process.env.PORT || 3000;
-
 
 app.listen(PORT, async () => {
   // Ensure the database is connected before starting the server
@@ -37,7 +26,7 @@ app.listen(PORT, async () => {
     await connectionToDB();
     console.log(`App is running at http://localhost:${PORT}`);
   } catch (error) {
-    console.error('Error connecting to the database:', error);
+    console.error("Error connecting to the database:", error);
   }
 });
 
--- middleware/auth.middleware.js
+++ middleware/auth.middleware.js
@@ -1,49 +1,45 @@
 import AppError from "../utils/error.utils.js";
-import jwt from 'jsonwebtoken';
+import jwt from "jsonwebtoken";
 import User from "../models/userModel.js";
 
 const isLoggedIn = async (req, res, next) => {
-  const { token } = req.cookies ;
+  const { token } = req.cookies;
 
-  console.log("login"+token);
+  console.log("login" + token);
 
   if (!token) {
-    console.log('No token found');
-    return next(new AppError('Unauthenticated, please login again', 400));
+    console.log("No token found");
+    return next(new AppError("Unauthenticated, please login again", 400));
   }
- 
+
   try {
     const userDetails = await jwt.verify(token, process.env.JWT_SECRET);
 
     req.user = userDetails;
-
   } catch (error) {
-    console.log('JWT verification failed:', error);
-    return next(new AppError('Invalid token or session expired', 401));
+    console.log("JWT verification failed:", error);
+    return next(new AppError("Invalid token or session expired", 401));
   }
   next();
-
 };
 
+const authorizeRoles =
+  (...roles) =>
+  async (req, res, next) => {
+    const currentUserRoles = req.user.role;
 
-const authorizeRoles = (...roles) => async (req, res, next) => {
-  
-  
-  const currentUserRoles=(req.user.role)
-  
-  if (!roles.includes(currentUserRoles)) {
-    return next(new AppError('You do not have permission to view this route', 403));
-  }
-console.log(authorizeRoles);
-  next();
-  
-
-};
+    if (!roles.includes(currentUserRoles)) {
+      return next(
+        new AppError("You do not have permission to view this route", 403)
+      );
+    }
+    console.log(authorizeRoles);
+    next();
+  };
 const authorizeSubscribers = async (req, _res, next) => {
   // If user is not admin or does not have an active subscription then error else pass
 
-
-  const user=await User.findById(req.user.id)
+  const user = await User.findById(req.user.id);
   if (user.role !== "ADMIN" && user.subscription.status !== "active") {
     return next(new AppError("Please subscribe to access this route.", 403));
   }
@@ -51,9 +47,4 @@ const authorizeSubscribers = async (req, _res, next) => {
   next();
 };
 
-
-export {
-  isLoggedIn,
-  authorizeRoles,
-  authorizeSubscribers
-};
+export { isLoggedIn, authorizeRoles, authorizeSubscribers };
